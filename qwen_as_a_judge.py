32ea2f5e4de1c6b9a34f23c8be6f1291ffc429bb
StanleyStanMarsh <iastafev56@gmail.com>
2026-05-25 01:28:40 +0300

    change model to Mistral-7B-Instruct-v0.2

--- qwen_as_a_judge.py
+++ qwen_as_a_judge.py
@@ -8,8 +8,8 @@ import time
 
 # Конфигурация
 VLLM_URL = "http://localhost:8000/v1/chat/completions"
-SMALL_MODEL_NAME = "Qwen2.5-7B-Instruct"
-MODEL = "Qwen/" + SMALL_MODEL_NAME
+SMALL_MODEL_NAME = "Mistral-7B-Instruct-v0.2"
+MODEL = "mistralai/" + SMALL_MODEL_NAME
 OUTPUT_DIR = f"{SMALL_MODEL_NAME}_results"
 
 # Создаем директорию для результатов, если нет
